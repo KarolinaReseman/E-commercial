68a8ac17113a4295bde11a514ff9dbc12742bf1b
KarolinaReseman <reseman.karolina@gmail.com>
2024-01-20 00:43:59 +0100

    added smalling.onclick function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
+const MainImg = document.getElementById("MainImg");
+const smalling = document.getElementsByClassName("small-img");
 
 if (bar) {
     bar.addEventListener('click', () => {
@@ -12,4 +14,20 @@ if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
     })
+}
+
+smalling[0].onclick = function() {
+    MainImg.src = smalling[0].src; 
+}
+smalling[1].onclick = function() {
+    MainImg.src = smalling[1].src; 
+
+}
+smalling[2].onclick = function() {
+    MainImg.src = smalling[2].src; 
+
+}
+smalling[3].onclick = function() {
+    MainImg.src = smalling[3].src; 
+
 }
