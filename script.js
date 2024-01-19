382bff28980ad93bc83ce7d7d9090c19d6dd7cb4
KarolinaReseman <reseman.karolina@gmail.com>
2024-01-20 00:44:35 +0100

    added ";"

--- script.js
+++ script.js
@@ -8,26 +8,26 @@ if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
     })
-}
+};
 
 if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
     })
-}
+};
 
 smalling[0].onclick = function() {
     MainImg.src = smalling[0].src; 
-}
+};
 smalling[1].onclick = function() {
     MainImg.src = smalling[1].src; 
 
-}
+};
 smalling[2].onclick = function() {
     MainImg.src = smalling[2].src; 
 
-}
+};
 smalling[3].onclick = function() {
     MainImg.src = smalling[3].src; 
 
-}
+};
